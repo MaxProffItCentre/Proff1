e340ea078c9db7e65587f6a97226624c06249fa8
Elena <Elena@Lipa>
2015-11-23 21:49:56 +0200

    HERE ARE THE BOYS

--- evgeny_plotnikov/src/main/java/action3/App.java
+++ evgeny_plotnikov/src/main/java/action3/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!1!sehfse" );
+        System.out.println( "Hello World!1!sehe" );
     }
 }
