393a13ae748bd926cf8b22f53a4ac9b8ae6d0759
Elena <Elena@Lipa>
2015-11-23 21:49:20 +0200

    NO PULL FOR OLD MEN

--- evgeny_plotnikov/src/main/java/action3/App.java
+++ evgeny_plotnikov/src/main/java/action3/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!1!sehfsekjfh" );
+        System.out.println( "Hello World!1!sehfse" );
     }
 }
