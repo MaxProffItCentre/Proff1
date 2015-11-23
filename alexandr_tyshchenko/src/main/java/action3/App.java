9fa4b19f59029639fd5088850e7fe870aff197e1
a-tyshchenko <a-tyshchenko@i.ua>
2015-11-23 21:51:02 +0200

    You must do it

--- alexandr_tyshchenko/src/main/java/action3/App.java
+++ alexandr_tyshchenko/src/main/java/action3/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Ay1y Hello World!" );
+        System.out.println( "Ay11y Hello World!" );
     }
 }
