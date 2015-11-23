437cd2f11562101717076e1c0ac325c6aef69ac0
katiezza <katiezza@gmail.com>
2015-11-23 21:41:30 +0200

    Commit at last!

--- kateryna_tkachuk/src/main/java/action3/App.java
+++ kateryna_tkachuk/src/main/java/action3/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Helllllllo World!" );
+        System.out.println( "Hellooo World!" );
     }
 }
