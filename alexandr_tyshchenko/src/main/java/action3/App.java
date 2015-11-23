4a4b83924d6efd6a1a83ca47343079eaf7b74f88
a-tyshchenko <a-tyshchenko@i.ua>
2015-11-23 22:01:50 +0200

    11111

--- alexandr_tyshchenko/src/main/java/action3/App.java
+++ alexandr_tyshchenko/src/main/java/action3/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Ay11y Hello World!" );
+        System.out.println( "1 Hello World!" );
     }
 }
