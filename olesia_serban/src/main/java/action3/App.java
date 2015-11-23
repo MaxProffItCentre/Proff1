5cee3665dc2c193a23661e01742d24ab01d8ef8f
Администратор <@DNAPC>
2015-11-23 21:51:32 +0200

    commit

--- olesia_serban/src/main/java/action3/App.java
+++ olesia_serban/src/main/java/action3/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "hjMy Hello World!" );
+        System.out.println( "hMy Hello World!" );
     }
 }
