1702ee62557f64c3789a6c4e0a0caf0b314c2a4c
alwizard <alwizard@alpk>
2015-11-23 21:56:31 +0200

    uuuuuiiii

--- andrii_logvyn/src/main/java/action3/App.java
+++ andrii_logvyn/src/main/java/action3/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worsdfgdsld!" );
+        System.out.println( "Hello Worlds!" );
     }
 }
