5301fa73aaaa54c92155a1d8464608de004947f7
Ivan <Ivan@Ivan-PC>
2015-11-24 18:43:58 +0200

    ...

--- ivan_purshega/ivan_purshega/src/main/java/action3/App.java
+++ ivan_purshega/ivan_purshega/src/main/java/action3/App.java
@@ -4,11 +4,9 @@ package action3;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hellllo World!" );
-        // Не пашет ХЕЛОУ ВОРЛД )))
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hellllo World!");
+		// Не пашет ХЕЛОУ ВОРЛД )))
+	}
 }
