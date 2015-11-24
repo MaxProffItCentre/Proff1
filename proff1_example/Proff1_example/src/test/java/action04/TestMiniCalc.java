3f054cd111d166093957f1a3a64d8717fd75338b
maksimtest <mkn_ua@ukr.net>
2015-11-24 22:26:40 +0200

    commit TestMiniCalc

---
+++ proff1_example/Proff1_example/src/test/java/action04/TestMiniCalc.java
@@ -0,0 +1,19 @@
+package action04;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMiniCalc {
+
+	@Test
+	public void test() {
+		MiniCalc app = new MiniCalc();
+		if(app.getMulti(2, 2) != 4)
+			fail("Not yet implemented");
+		
+		assertTrue("Oy!",app.getMulti(3, 2) == 6);
+		assertEquals(new Object(),new Object());
+	}
+
+}
