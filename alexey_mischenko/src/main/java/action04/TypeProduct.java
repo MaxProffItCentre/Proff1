935010e0997c548d93257fe13e1511f20f787fa4
Alexey <alexeymi@ukr.net>
2015-11-24 20:40:24 +0200

    2

--- alexey_mischenko/src/main/java/action04/TypeProduct.java
+++ alexey_mischenko/src/main/java/action04/TypeProduct.java
@@ -1,5 +1,8 @@
 package action04;
 
+import java.util.Map;
+import java.util.Set;
+
 public enum TypeProduct {
 	Bread, Yoqurt, Milk;
 	
@@ -13,6 +16,16 @@ public enum TypeProduct {
 		TypeProduct obj2 = obj1;
 		TypeProduct [] arr = TypeProduct.values();
 		System.out.println(obj2);
+		
+		Map<Integer, String> map = new HashMap<Integer, String>();
+		map.put(10, "Stroka1");
+		map.put(20, "Stroka2");
+		
+		
+		Set<Map.Entry<Integer, String>> set = map.entrySet();
+		for(Map.Entry<Integer, String> entry: set){
+			System.out.println(entry.getKey() +"="+ entry.getValue());
+		}
 		
 		
 	}
