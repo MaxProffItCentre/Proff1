28a3670bc6310cb3907630e7e4900ac6b8fbbe83
Admin <Admin@microsof-3cab44>
2015-11-27 00:05:47 +0300

    comit

--- ivan_purshega/ivan_purshega/src/main/java/action03/App.java
+++ ivan_purshega/ivan_purshega/src/main/java/action03/App.java
@@ -6,7 +6,7 @@ package action03;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hellllo World!!!!");
+		System.out.println("Helll lo World!!!!");
 		// Не пашет ХЕЛОУ ВОРЛД )))
 	}
 }
