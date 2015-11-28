72cd9688a225af888c53908150c11d3712237ad5
maksimtest <mkn_ua@ukr.net>
2015-11-28 17:28:01 +0200

    commit action04 after ReBuild Project!

---
+++ proff1_example/src/main/java/action04/ProductShop.java
@@ -0,0 +1,69 @@
+package action04;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+
+public class ProductShop {
+	Map<Product,String> contructors = new HashMap<Product,String>();
+	public ProductShop(){
+		contructors.put(new Product("Bread White",TypeProduct.Bread, 10),"Firma 3");
+		contructors.put(new Product("Super Yogurt",TypeProduct.Yogurt, 10),"Firma 3");
+		
+		contructors.put(new Product("Bread Grey",TypeProduct.Bread, 12),"Firma 3");
+		contructors.put(new Product("Yogurt for Java Developers",TypeProduct.Yogurt, 12),"Firma 3");
+		
+		contructors.put(new Product("Milk with Shokolate",TypeProduct.Milk, 14),"Firma 1");
+		contructors.put(new Product("Simple Yogurt",TypeProduct.Yogurt, 14),"Firma 1");
+		contructors.put(new Product("Bread Grey",TypeProduct.Bread, 16),"Firma 1");
+		contructors.put(new Product("Yogurt for Java Developers",TypeProduct.Yogurt, 16),"Firma 1");
+		contructors.put(new Product("Milk with Shokolate",TypeProduct.Milk, 14),"Firma 1");
+		contructors.put(new Product("Simple Yogurt",TypeProduct.Yogurt, 14),"Firma 1");
+		contructors.put(new Product("Bread Grey",TypeProduct.Bread, 16),"Firma 1");
+		contructors.put(new Product("Bread Grey",TypeProduct.Bread, 18),"Firma 1");
+
+		contructors.put(new Product("Bread White",TypeProduct.Bread, 20),"Firma 2");
+		contructors.put(new Product("Super Yogurt",TypeProduct.Yogurt, 20),"Firma 2");
+		contructors.put(new Product("Bread Grey",TypeProduct.Bread, 20),"Firma 2");
+		contructors.put(new Product("Yogurt for Java Developers",TypeProduct.Yogurt, 20),"Firma 2");
+		
+	}
+	public static void main(String[] args) {
+		ProductShop shop = new ProductShop();
+		shop.printContructor();
+		shop.printReverseContructor();
+		shop.printNameProductByParty(TypeProduct.Yogurt);
+	}
+	/*
+	+	 1) ¬ывести уникальный список фирм в алфавитном пор€дке 
+	+	2) ¬ывести уникальный список фирм в обратном пор€дке
+	+	3) ¬ывести уникальные наименовани€ товаров по типу продуктов
+	+	 * */
+	public void printContructor(){
+		Set<String> set = new TreeSet<String>(contructors.values());
+		System.out.println(set);
+	}
+	public void printReverseContructor(){
+		Set<String> set = new HashSet<String>(contructors.values());
+		List<String> list = new ArrayList<String>(set);
+		Collections.sort(list,Collections.reverseOrder());
+		System.out.println(list);
+	}
+	public void printNameProductByParty(TypeProduct type){
+		Set<Map.Entry<Product, String>> set = contructors.entrySet();
+		
+		Set<String> products = new HashSet<String>();
+		for(Map.Entry<Product, String> entry:set){
+			Product product = entry.getKey();
+			if(product.getType() == type)products.add(product.getName());
+		}
+		System.out.println(products);
+	}
+}
+
