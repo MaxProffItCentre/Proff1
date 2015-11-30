c429d4028ff6e85a5cb64b3690de95609f96fb29
maksimtest <mkn_ua@ukr.net>
2015-11-30 20:51:47 +0200

    commit JavaFxExample

--- proff1_example/src/main/java/action05/JavaFxExample.java
+++ proff1_example/src/main/java/action05/JavaFxExample.java
@@ -1,5 +1,16 @@
 package action05;
 
 public class JavaFxExample extends Application{
-
+	public static void main(String[] args) {
+		launch(args);
+	}
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("Первое приложение");
+		stage.setScene(createScene());
+		stage.show();
+	}
+	public Scene createScene(){
+		return null;
+	}
 }
