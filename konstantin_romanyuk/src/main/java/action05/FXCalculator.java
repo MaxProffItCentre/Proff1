f0bc203c14a4dc7ed1484bbe1872962955d14b60
winPC <winPC@192.168.0.36>
2015-12-01 13:32:42 +0200

    Reflection+Calculator

---
+++ konstantin_romanyuk/src/main/java/action05/FXCalculator.java
@@ -0,0 +1,90 @@
+package action05;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+/*Задача4: Калькулятор:
+TextField ( number1)
+TextField ( operation)
+TextField ( number2)
+Button ( =)
+TextField ( result)*/
+public class FXCalculator extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("Calculator");
+		stage.setScene(createScene1());
+		stage.show();
+		
+	}
+	public Scene createScene1(){
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.TOP_LEFT);
+		grid.setHgap(20);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25, 25, 25, 25));
+		//
+		Label label1 = new Label("Comment 1");
+		label1.setLayoutX(5);
+		label1.setLayoutY(14);
+		//
+		TextField text = new TextField("");
+		text.setLayoutX(20);
+		text.setLayoutY(5);
+		text.setMinWidth(80);
+		
+		
+		Button button1 = new Button(" = ");
+		button1.setOnAction(new EventHandler(){			
+			public void handle(Event arg0) {
+				text.appendText(button1.getText());
+			
+			
+			}});
+		
+		//
+		
+		//text.setEditable(false);
+		//
+		grid.add(text, 0, 0);
+		grid.add(button1, 1, 1);
+		
+		grid.add(new Button("1"), 1, 2);
+		grid.add(new Button("2"), 1, 3);
+		grid.add(new Button("3"), 1, 4);
+		
+		//		
+		Scene scene = new Scene(grid, 200, 200);
+		return scene;
+
+}
+	/*public class MyButton extends Button{
+		Button bb;
+		
+		public MyButton(String txt){
+			bb= new Button(txt);
+			bb.setOnAction(new EventHandler(){			
+				public void handle(Event arg0) {
+					result=txt;
+				}});*/
+
+				
+			
+		//}
+	//}
+}
