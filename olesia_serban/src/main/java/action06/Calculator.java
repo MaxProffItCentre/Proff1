30b57077877ba6d168a2c8c71842eeb3b79180e8
Администратор <@DNAPC>
2015-12-04 23:32:41 +0200

    Calculator commit

--- olesia_serban/src/main/java/action06/Calculator.java
+++ olesia_serban/src/main/java/action06/Calculator.java
@@ -23,76 +23,74 @@ public class Calculator extends Application {
 		stage.setScene(scene());
 		stage.show();
 	}
-	
-	public Scene scene(){
-		
-		//text field for the first number
+
+	public Scene scene() {
+
+		// text field for the first number
 		TextField number1 = new TextField();
 		number1.setMaxSize(100, 20);
 		number1.setEditable(true);
-		
-		//text field for operation sign
+
+		// text field for operation sign
 		TextField operation = new TextField();
 		operation.setMaxSize(50, 20);
 		operation.setEditable(true);
-		
-		//text field for the second number
+
+		// text field for the second number
 		TextField number2 = new TextField();
 		number2.setMaxSize(100, 20);
 		number2.setEditable(true);
-		
-		//Button for "="
+
+		// Button for "="
 		Button btn = new Button("=");
 		btn.setMaxSize(60, 20);
-		
-		//text field for result
+
+		// text field for result
 		TextField result = new TextField();
 		result.setMaxSize(150, 20);
 		result.setEditable(false);
 		result.setMouseTransparent(true);
-		
-		//Event for my button
-		class MyEvent<T> implements EventHandler{
+
+		// Event for my button
+		class MyEvent<T> implements EventHandler {
 			TextField num1;
 			TextField action;
 			TextField num2;
 			TextField result;
-			
-			public MyEvent(TextField num1,TextField action,	TextField num2,TextField result){
-				this.num1=num1;
+
+			public MyEvent(TextField num1, TextField action, TextField num2, TextField result) {
+				this.num1 = num1;
 				this.num2 = num2;
-				this.action=action;
-				this.result=result;
-				
-							
+				this.action = action;
+				this.result = result;
+
 			}
-			public String result(String num1, String num2, String action){
-				if(action.equals("+")){
-					return new Double(num1)+new Double(num2)+"";
-				}else if(action.equals("-")){
-					return new Double(num1)-new Double(num2)+"";
-				}else if(action.equals("*")){
-					return new Double(num1)*new Double(num2)+"";
-				}else if(action.equals("/")){
-					return new Double(num1)/new Double(num2)+"";
-				}else{
+
+			public String result(String num1, String num2, String action) {
+				if (action.equals("+")) {
+					return new Double(num1) + new Double(num2) + "";
+				} else if (action.equals("-")) {
+					return new Double(num1) - new Double(num2) + "";
+				} else if (action.equals("*")) {
+					return new Double(num1) * new Double(num2) + "";
+				} else if (action.equals("/")) {
+					return new Double(num1) / new Double(num2) + "";
+				} else {
 					return "Invalid operation";
 				}
-				
-				
+
 			}
-			
 
 			public void handle(Event event) {
 				result.setText(result(num1.getText(), num2.getText(), action.getText()));
 				System.out.println("=)");
-				
+
 			}
-			
+
 		}
-		//add all elements to grid
+		// add all elements to grid
 		GridPane grid = new GridPane();
-		grid.setPadding(new Insets(10,10,10,10));
+		grid.setPadding(new Insets(10, 10, 10, 10));
 		grid.setAlignment(Pos.CENTER_LEFT);
 		grid.setHgap(20);
 		grid.add(number1, 0, 0);
@@ -100,13 +98,13 @@ public class Calculator extends Application {
 		grid.add(number2, 2, 0);
 		grid.add(btn, 3, 0);
 		grid.add(result, 4, 0);
-		
-		//setting button on action
+
+		// setting button on action
 		btn.setOnAction(new MyEvent(number1, operation, number2, result));
-		
+
 		Scene scene = new Scene(grid, 550, 200);
 		return scene;
-		
+
 	}
 
 }
