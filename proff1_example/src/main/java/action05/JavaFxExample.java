48fa5ea3d5a2cf10e22120fd4f5e11ed0f9ccb87
admin <email@ukr.net>
2016-01-12 02:40:36 +0200

    update pom.xml

--- proff1_example/src/main/java/action05/JavaFxExample.java
+++ proff1_example/src/main/java/action05/JavaFxExample.java
@@ -1,11 +1,8 @@
 package action05;
 
-<<<<<<< HEAD
-=======
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
->>>>>>> branch 'master' of https://github.com/MaxProffItCentre/Proff1.git
 
 public class JavaFxExample extends Application{
 	public static void main(String[] args) {
