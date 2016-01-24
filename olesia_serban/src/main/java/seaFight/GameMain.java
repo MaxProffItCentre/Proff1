f9d56e2388a724ee1cf241bc5725a02849e6e574
Olesia <OlesiaSerban@gmail.com>
2016-01-24 10:12:15 +0200

    game's over!

---
+++ olesia_serban/src/main/java/seaFight/GameMain.java
@@ -0,0 +1,13 @@
+package seaFight;
+
+
+import java.io.IOException;
+
+public class GameMain {
+	public static void main(String[] args) throws IOException {
+		Game game = new Game();
+		game.gameInit();
+//		File htmlFile = new File("C:/java/repo/Proff1/olesia_serban/src/main/java/seaFight/index.html");
+//		Desktop.getDesktop().browse(htmlFile.toURI());
+	}
+}
